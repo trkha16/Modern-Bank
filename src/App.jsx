ad49895a2979adef4a97250f6d9489db8a20d7aa
trkha16 <tbkha1609@gmail.com>
2022-08-25 01:20:33 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,47 @@
 import React from "react";
+import Billing from "./components/Billing";
+import Business from "./components/Business";
+import CardDeal from "./components/CardDeal";
+import Clients from "./components/Clients";
+import CTA from "./components/CTA";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Stats from "./components/Stats";
+import Testimonials from "./components/Testimonials";
+import styles from "./style";
 
 const App = () => {
-    return <div>App</div>;
+    return (
+        <div className="bg-primary w-full overflow-hidden text-white">
+            <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+                <div className={`${styles.boxWidth}`}>
+                    <Navbar></Navbar>
+                </div>
+            </div>
+
+            <div className={`bg-primary ${styles.flexStart}`}>
+                <div className={`${styles.boxWidth}`}>
+                    <Hero></Hero>
+                </div>
+            </div>
+
+            <div
+                className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}
+            >
+                <div className={`${styles.boxWidth}`}>
+                    <Stats></Stats>
+                    <Business></Business>
+                    <Billing></Billing>
+                    <CardDeal></CardDeal>
+                    <Testimonials></Testimonials>
+                    <Clients></Clients>
+                    <CTA></CTA>
+                    <Footer></Footer>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default App;
---
+++ src/components/Business.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { features } from "../constants";
+import styles, { layout } from "../style";
+import Button from "./Button";
+
+const Business = () => {
+    return <div>Business</div>;
+};
+
+export default Business;
