ddde91aae50f721346a25d3a83e0c6c369657294
trkha16 <tbkha1609@gmail.com>
2022-08-25 23:42:07 +0700

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
                         key={nav.id}
                         className={`font-poppins font-normal cursor-pointer text-[16px] text-white ${
                             index === navLinks.length - 1 ? "mr-0" : "mr-10"
-                        }`}
+                        } hover:text-secondary`}
                     >
                         <a href={`#${nav.id}`}>{nav.title}</a>
                     </li>
--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -11,7 +11,7 @@ const Stats = () => {
             {stats.map((stat) => (
                 <div
                     key={stat.id}
-                    className={`flex-1 flex justify-start items-center flex-row m-3`}
+                    className={`flex-1 flex xs:justify-center items-center flex-row m-3`}
                 >
                     <h4 className="font-poppins font-semibold xs:text-[40px] text-[30px] xs:leading-[53px] leading-[43px] text-white">
                         {stat.value}
